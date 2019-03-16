f52b2fb07d69b5355863b80a72988346f27ae52a
Gabriela Ordonez <00361516@uca.edu.sv>
2019-03-16 11:05:51 -0600

    Ejercicio 3

--- app/src/main/java/com/gordonez/ej3/MainActivity.java
+++ app/src/main/java/com/gordonez/ej3/MainActivity.java
@@ -2,12 +2,27 @@ package com.gordonez.ej3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView redColor = findViewById(R.id.color_red);
+        TextView greenColor = findViewById(R.id.color_green);
+        TextView blueColor = findViewById(R.id.color_blue);
+
+        redColor.setOnClickListener(this);
+        blueColor.setOnClickListener(this);
+        greenColor.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
